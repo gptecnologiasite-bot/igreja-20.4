6d967630f58d35dec201475b2cd3746c2105b471
humberto <gptecnologiasite@gmail.com>
2026-03-03 09:56:43 -0300

    painel e site  atualizado

---
+++ src/utils/imageUtils.js
@@ -0,0 +1,31 @@
+/**
+ * Transform standard Google Drive sharing links into direct image links
+ * format: https://drive.google.com/uc?export=download&id=FILE_ID
+ */
+export const transformImageLink = (url) => {
+  if (!url || typeof url !== 'string') return url;
+
+  // Handle Google Drive links
+  if (url.includes('drive.google.com')) {
+    let fileId = '';
+    
+    // Pattern: /file/d/[ID]/view
+    const driveMatch = url.match(/\/file\/d\/([a-zA-Z0-9_-]+)/);
+    if (driveMatch && driveMatch[1]) {
+      fileId = driveMatch[1];
+    } 
+    // Pattern: id=[ID]
+    else {
+      const idMatch = url.match(/id=([a-zA-Z0-9_-]+)/);
+      if (idMatch && idMatch[1]) {
+        fileId = idMatch[1];
+      }
+    }
+
+    if (fileId) {
+      return `https://drive.google.com/uc?export=download&id=${fileId}`;
+    }
+  }
+
+  return url;
+};
